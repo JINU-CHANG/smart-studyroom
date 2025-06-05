a236011a26182f95586d4f11d5422368895b17da
jinwoo22 <jenny010925@naver.com>
2025-06-05 13:33:30 +0900

    chore: 스프링 서버 생성

---
+++ server/src/test/java/com/IoT/smartstudyroom/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IoT.smartstudyroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
